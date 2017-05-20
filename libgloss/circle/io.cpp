04c5a8cd16ddf97d147375be2c1044b3a7d5c1f7
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2017-05-20 20:41:44 +0200

    Moved I/O functions into new io.cpp module.

--- libgloss/circle/io.cpp
+++ libgloss/circle/io.cpp
@@ -1,4 +1,75 @@
+#include "config.h"
+#include <_ansi.h>
+#include <_syslist.h>
+#include <errno.h>
+#undef errno
+extern int errno;
+#include "warning.h"
 
-/* Just a non-empty compilation unit for now... */
-class non_empty {
-} ne;
+/*
+ * Stub version of open.
+ */
+
+extern "C"
+int
+_DEFUN (_open, (file, flags, mode),
+        char *file  _AND
+        int   flags _AND
+        int   mode)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+stub_warning(_open)
+
+/*
+ * Stub version of close.
+ */
+
+extern "C"
+int
+_DEFUN (_close, (fildes),
+        int fildes)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+stub_warning (_close)
+
+/*
+ * Stub version of read.
+ */
+
+extern "C"
+int
+_DEFUN (_read, (file, ptr, len),
+        int   file  _AND
+        char *ptr   _AND
+        int   len)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+stub_warning(_read)
+
+
+/*
+ * Stub version of write.
+ */
+
+extern "C"
+int
+_DEFUN (_write, (file, ptr, len),
+        int   file  _AND
+        char *ptr   _AND
+        int   len)
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+stub_warning(_write)
+
