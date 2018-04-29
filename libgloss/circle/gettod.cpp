9af404228a7dda4cd6afc8aded6801efe5cae792
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2018-04-29 21:56:27 +0200

    Use atomic method of Circle to get time
    Get seconds and microseconds in a single atomic operation
    from Circle.

--- libgloss/circle/gettod.cpp
+++ libgloss/circle/gettod.cpp
@@ -8,6 +8,8 @@
 #include <sys/time.h>
 #include <sys/times.h>
 #include <errno.h>
+#undef errno
+extern int errno;
 #include "warning.h"
 
 #include <circle/timer.h>
@@ -18,8 +20,22 @@ _DEFUN (_gettimeofday, (ptimeval, ptimezone),
         struct timeval *ptimeval _AND
         void *ptimezone)
 {
-        ptimeval->tv_sec = CTimer::Get ()->GetUniversalTime ();
-        ptimeval->tv_usec = 0;
+        (void) ptimezone;
 
-        return 0;
+        unsigned seconds, microSeconds;
+        int result;
+
+        if (CTimer::Get ()->GetUniversalTime (&seconds, &microSeconds))
+        {
+                ptimeval->tv_sec = static_cast<time_t>(seconds);
+                ptimeval->tv_usec = static_cast<suseconds_t>(microSeconds);
+                result = 0;
+        }
+        else
+        {
+                errno = EINVAL;
+                result = -1;
+        }
+
+        return result;
 }
