70f02953121d01c95cf3b5feb83ad93cf0784f73
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2025-07-28 22:21:03 +0200

    Remove duplicate macro
    This now comes centrally from "circle_macros.h".

--- libgloss/circle/_exit.cpp
+++ libgloss/circle/_exit.cpp
@@ -5,10 +5,6 @@
 #include "circle_macros.h"
 #include <_ansi.h>
 #include <_syslist.h>
-
-// Workaround for use of ASSERT_STATIC from Circle's assert.h,
-// which is not available in the newlib headers.
-#define ASSERT_STATIC(expr) static_assert (expr, #expr)
 #include <circle/startup.h>
 
 extern "C"
