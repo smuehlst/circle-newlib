74b9aa1346aedc7eec4529163ddef5582045e1e0
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2025-10-18 20:50:52 +0200

    Fix typo

--- libgloss/circle/circle_macros.h
+++ libgloss/circle/circle_macros.h
@@ -1,7 +1,7 @@
 #ifndef CIRCLE_MACROS_H
 #define CIRCLE_MACROS_H
 
-// Define macros that are defined amd used in Circle's headers,
+// Define macros that are defined and used in Circle's headers,
 // but not in the newlib headers.
 
 #define ASSERT_STATIC(expr) static_assert(expr, #expr)
