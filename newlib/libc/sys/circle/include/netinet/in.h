28f97639c79cafac322e7eb2696176d80c240336
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2025-11-04 22:29:10 +0100

    Add further definitions
    This is only for being able to compile applications at the moment,
    nothing of this is implemented.

--- newlib/libc/sys/circle/include/netinet/in.h
+++ newlib/libc/sys/circle/include/netinet/in.h
@@ -4,31 +4,87 @@
 #include <inttypes.h>
 #include <bits/sockettypes.h>
 
-typedef	uint16_t in_port_t;
+typedef uint16_t in_port_t;
 typedef uint32_t in_addr_t;
 
-struct in_addr {
+struct in_addr
+{
     in_addr_t s_addr;
 };
 
 /* The sin_port and sin_addr members shall be in network byte order (big-endia). */
-struct sockaddr_in {
-    sa_family_t sin_family; 
+struct sockaddr_in
+{
+    sa_family_t sin_family;
     in_port_t sin_port;
     struct in_addr sin_addr;
 };
 
-#define IPPROTO_IP      1                   /* Internet protocol. */
-#define IPPROTO_IPV6    (IPPROTO_IP + 1)    /* Internet Protocol Version 6. */
-#define IPPROTO_ICMP    (IPPROTO_IP + 2)    /* Control message protocol. */
-#define IPPROTO_RAW     (IPPROTO_IP + 3)    /* Raw IP Packets Protocol. */
-#define IPPROTO_TCP     (IPPROTO_IP + 4)    /* Transmission control protocol. */
-#define IPPROTO_UDP     (IPPROTO_IP + 5)    /* User datagram protocol. */
+/* Multicast not implemented yet, only for being able to compile currently. */
+struct ip_mreq
+{
+    struct in_addr imr_multiaddr;
+    struct in_addr imr_interface;
+};
 
-#define	INADDR_ANY		((in_addr_t) 0x00000000) /* IPv4 wildcard address. */
-#define	INADDR_LOOPBACK	((in_addr_t) 0x7f000001) /* IPv4 local host address. */
-#define	INADDR_BROADCAST ((in_addr_t) 0xffffffff) /* IPv4 broadcast address. */
+struct ip_mreqn
+{
+    struct in_addr imr_multiaddr;
+    struct in_addr imr_address;
+    int imr_ifindex;
+};
 
-#define INET_ADDRSTRLEN 16                  /* Length of the string form for IP. */
+struct ip_mreq_source
+{
+    struct in_addr imr_multiaddr;
+    struct in_addr imr_interface;
+    struct in_addr imr_sourceaddr;
+};
+
+#define IPPROTO_IP 1                  /* Internet protocol. */
+#define IPPROTO_IPV6 (IPPROTO_IP + 1) /* Internet Protocol Version 6. */
+#define IPPROTO_ICMP (IPPROTO_IP + 2) /* Control message protocol. */
+#define IPPROTO_RAW (IPPROTO_IP + 3)  /* Raw IP Packets Protocol. */
+#define IPPROTO_TCP (IPPROTO_IP + 4)  /* Transmission control protocol. */
+#define IPPROTO_UDP (IPPROTO_IP + 5)  /* User datagram protocol. */
+
+#define INADDR_ANY ((in_addr_t)0x00000000)       /* IPv4 wildcard address. */
+#define INADDR_LOOPBACK ((in_addr_t)0x7f000001)  /* IPv4 local host address. */
+#define INADDR_BROADCAST ((in_addr_t)0xffffffff) /* IPv4 broadcast address. */
+
+#define INET_ADDRSTRLEN 16 /* Length of the string form for IP. */
+
+#define IP_ADD_MEMBERSHIP 1
+#define IP_ADD_SOURCE_MEMBERSHIP (IP_ADD_MEMBERSHIP + 1)
+#define IP_BIND_ADDRESS_NO_PORT (IP_ADD_MEMBERSHIP + 2)
+#define IP_BLOCK_SOURCE (IP_ADD_MEMBERSHIP + 3)
+#define IP_DROP_MEMBERSHIP (IP_ADD_MEMBERSHIP + 4)
+#define IP_DROP_SOURCE_MEMBERSHIP (IP_ADD_MEMBERSHIP + 5)
+#define IP_FREEBIND (IP_ADD_MEMBERSHIP + 6)
+#define IP_HDRINCL (IP_ADD_MEMBERSHIP + 7)
+#define IP_LOCAL_PORT_RANGE (IP_ADD_MEMBERSHIP + 8)
+#define IP_MSFILTER (IP_ADD_MEMBERSHIP + 9)
+#define IP_MTU (IP_ADD_MEMBERSHIP + 10)
+#define IP_MTU_DISCOVER (IP_ADD_MEMBERSHIP + 11)
+#define IP_MULTICAST_ALL (IP_ADD_MEMBERSHIP + 12)
+#define IP_MULTICAST_IF (IP_ADD_MEMBERSHIP + 13)
+#define IP_MULTICAST_LOOP (IP_ADD_MEMBERSHIP + 14)
+#define IP_MULTICAST_TTL (IP_ADD_MEMBERSHIP + 15)
+#define IP_NODEFRAG (IP_ADD_MEMBERSHIP + 16)
+#define IP_OPTIONS (IP_ADD_MEMBERSHIP + 17)
+#define IP_PASSSEC (IP_ADD_MEMBERSHIP + 18)
+#define IP_PKTINFO (IP_ADD_MEMBERSHIP + 19)
+#define IP_RECVERR (IP_ADD_MEMBERSHIP + 20)
+#define IP_RECVOPTS (IP_ADD_MEMBERSHIP + 21)
+#define IP_RECVORIGDSTADDR (IP_ADD_MEMBERSHIP + 22)
+#define IP_RECVTOS (IP_ADD_MEMBERSHIP + 23)
+#define IP_RECVTTL (IP_ADD_MEMBERSHIP + 24)
+#define IP_RETOPTS (IP_ADD_MEMBERSHIP + 25)
+#define IP_ROUTER_ALERT (IP_ADD_MEMBERSHIP + 26)
+#define IP_TOS (IP_ADD_MEMBERSHIP + 27)
+#define IPIP_TRANSPARENT_TOS (IP_ADD_MEMBERSHIP + 28)
+#define IP_TTL (IP_ADD_MEMBERSHIP + 29)
+#define IP_UNBLOCK_SOURCE (IP_ADD_MEMBERSHIP + 30)
+#define SO_PEERSEC (IP_ADD_MEMBERSHIP + 31)
 
 #endif
