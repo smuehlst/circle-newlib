484bbcb2dca8aa19c23ae807e0019694fdf1f464
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2026-04-02 19:40:58 +0200

    Implement clock_gettime() for libc++ threads

---
+++ libgloss/circle/clock_gettime.cpp
@@ -0,0 +1,36 @@
+#define _POSIX_TIMERS 1
+#include "config.h"
+#include <errno.h>
+#include <time.h>
+
+#include "circle_macros.h"
+#include <circle/timer.h>
+
+extern "C" int
+clock_gettime(clockid_t clock_id, struct timespec *tp)
+{
+    if (clock_id == CLOCK_MONOTONIC)
+    {
+        u64 const us = CTimer::GetClockTicks64();
+        tp->tv_sec = static_cast<time_t>(us / 1000000ULL);
+        tp->tv_nsec = static_cast<long>((us % 1000000ULL) * 1000ULL);
+        return 0;
+    }
+
+    if (clock_id == CLOCK_REALTIME)
+    {
+        unsigned sec = 0;
+        unsigned us = 0;
+        if (CTimer::Get()->GetUniversalTime(&sec, &us))
+        {
+            tp->tv_sec = static_cast<time_t>(sec);
+            tp->tv_nsec = static_cast<long>(us) * 1000L;
+            return 0;
+        }
+        errno = EINVAL;
+        return -1;
+    }
+
+    errno = EINVAL;
+    return -1;
+}
